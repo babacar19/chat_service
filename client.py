4c4269958ae627b6de72a1be495d895c17960b4b
Babacar NDIAYE <ndiayebaba007@gmail.com>
2020-04-23 10:23:35 +0200

    probleme de chat sensiblement resolu ; reste à voir des exceptions et la gestion des threads

--- client.py
+++ client.py
@@ -1,12 +1,32 @@
 import socket
 import sys
-from info_client import *
+import pickle
 import threading
 
 """ gestion des paramètres par defaut """
 
+#-----------------------------------
+""" definiition d'un moniteur pour tenter de gerer le parallelisme entre la reception et la messagerie """
+
+
+class moniteur_client():
+    def __init__(self):
+        self.tmp = 1
+        self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
+
+#---------------------------------------------------------
+moniteur = moniteur_client()
 mutex = threading.Semaphore(1)
 socket_connection = 0
+
+class message:
+    def __int__(self,msg=None,target = None):
+        self.message = msg
+        self.target = target
+    def pick_msg(self):
+        return pickle.dumps(self)
+
 
 class Receveur(threading.Thread):
 
@@ -17,15 +37,16 @@ class Receveur(threading.Thread):
     def run(self):
         global socket_connection
         #mutex.acquire()
-        print("reception message ..........")
+
         try:
-            data = socket_connection.recv(1024)
+            data = socket_connection.recv(4096)
+            print("reception message ..........")
         except ConnectionAbortedError:
             print("le server n'est plus en marche . Connection finie ")
             exit(1)
 
-        data = data.decode('utf-8')
-        print(">>>>>:received:  ",data )
+        msg = pickle.loads(data)
+        print(">>>>>:received:  "+ msg.message )
         #mutex.release()
 
 
@@ -37,18 +58,21 @@ class Envoyeur(threading.Thread):
 
     def run(self):
         global socket_connection
-        mutex.acquire()
-        with my_lock:
-            data = input(">>>>:  ")
-            try:
-                socket_connection.sendall(data.encode('utf-8'))
-            except ConnectionAbortedError:
-                print("le serveur ne marche plus . fin de la connection")
-                exit(1)
+        moniteur.lock.acquire()
 
-            print(">>>:message envoyé ")
-            #End
-        mutex.release()
+        m = message()
+        m.target = input(" >>> ip target ?? ... : ")
+        m.message = input(">>>> message:  ")
+        data = m.pick_msg()
+        try:
+            socket_connection.sendall(data)
+        except ConnectionAbortedError:
+            print("le serveur ne marche plus . fin de la connection")
+            exit(1)
+
+        print(">>>:message envoyé ")
+        #End
+        moniteur.lock.release()
 #---------------------------------------------------------------------------------#
 
 ip_sever = '10.188.4.88'
--- server.py
+++ server.py
@@ -1,4 +1,58 @@
 import socket #module necessaire pour la creation et utilisation des sockets
+import threading
+import  select
+import pickle
+#-------------------------------------------------
+"""
+
+--le serveur ne fait que recevoir des requêtes de message préfixées par un @ suivi du nom de la machine cible
+--Le serveur doit aussi mettre à jour tous les clients sur le nombre de clients connectés
+
+"""
+
+"""
+
+dico [ (ip , conn , id )]
+
+"""
+
+
+class message:
+    def __int__(self,msg=None,target = None):
+        self.message = msg
+        self.target = target
+    def pick_msg(self):
+        return pickle.dumps(self)
+
+
+clients_connectes = 0
+
+class Receveur(threading.Thread):
+    def __int__(self, socClient, clients):
+        threading.Thread.__init__(self)
+        self.sockClient = socClient
+        self.client_connectes = clients
+
+    def run(self):
+
+        data = self.sockClient.recv(4096)
+        msg = pickle.loads(data)
+        print("message recu : " + msg.message)
+        """"""
+        target = msg.target
+        for client in self.client_connectes:
+            print("comparaison ",client[1][0],target)
+            if client[1] == target:
+                msg_obj = msg.pick_msg()
+                client[0].sendall(msg_obj)
+            break
+        pass
+
+
+
+
+
+#--------------------------------------------------
 
 
 host, port = ('', 5566 ) #on definit l'adresse de l'hote et le port que l'on va utiliser
@@ -6,24 +60,52 @@ host, port = ('', 5566 ) #on definit l'adresse de l'hote et le port que l'on va 
 socket_reception = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creation de notre socket
 socket_reception.bind((host, port)) #on attache notre socket à une adresse en préécisant le port
 
-print("creation de socket et bindind complete ")
+print("creation de socket et binding du socket ")
 
 
 fin = 0
 
 socket_reception.listen(5) #le nombre donné en argument correspond le nombre d'essais disponible avant de refuser la conection ; cette methode est un appel bloquant
 print("serveur en ecoute ")
-while True and fin != 1:
 
-    conn, adr = socket_reception.accept()#on accepte la connection; cette méthode renvoie un tuple contenant la connection et l'adresse
+
+serveurLance = True
+connection_en_cours = []
+
+
+while serveurLance:
+
+    #ecoutons les clients connectés avec select
+    clients_connectes,wlist, xlist = select.select([socket_reception],[],[],9)
+
+    for client in clients_connectes:
+        my_client , host = client.accept()
+        connection_en_cours.append((my_client,host[0]))
+
+    for client in connection_en_cours:
+        if client:
+
+            traiteur = Receveur()
+            traiteur.sockClient = client[0]
+            traiteur.client_connectes = connection_en_cours
+            traiteur.start()
+
+
+
+    """for client in client_a_traiter:
+        traiteur = Receveur(client[0] ,client[1])
+        traiteur.start()"""
+
+    """conn, adr = socket_reception.accept()#on accepte la connection; cette méthode renvoie un tuple contenant la connection et l'adresse
     print("connection établie ")
     while fin!=1:
         #reception de la donnee encodée
-        data = conn.recv(1024) #cette méthode prend en parameetre la taille du buffer , mais aussi des valeurs pour les flags , cf documentation
-        if data == b'fin':
-            fin = 1
-            #endIf
+
         try:
+            data = conn.recv(1024)  # cette méthode prend en parameetre la taille du buffer , mais aussi des valeurs pour les flags , cf documentation
+            if data == b'fin':
+                fin = 1
+                # endIf
             data = data.decode('utf-8')
         except ConnectionResetError:
             print(" connection coupée ")
@@ -31,9 +113,9 @@ while True and fin != 1:
 
         print(data)
         msg= "reçu 5/5 ".encode('utf-8')
-        conn.sendall(msg)
+        conn.sendall(msg)"""
     #end (...for the moment )
 
 
-conn.close()
+
 socket_reception.close()
